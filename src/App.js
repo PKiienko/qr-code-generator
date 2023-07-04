85eb788f9980a1d29080c5a7225e6b546e2d594b
PKiienko <petro.kiienko@gmail.com>
2023-07-04 12:28:25 +0300

    download works

--- src/App.js
+++ src/App.js
@@ -1,64 +1,75 @@
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import { renderToString } from 'react-dom/server';
 
 import './App.css';
 
 const App = () => {
   const [inputText, setInputText] = useState('');
   const [size, setSize] = useState(200);
-  const [isShowCode, setIsShowCode] = useState(false);
 
-  const showCode = () => {
-    setIsShowCode(true);
+  const saveQR = () => {
+    const svgString = renderToString(<QRCode size={size} value={inputText} />);
+    const canvas = document.createElement('canvas');
+    const img = new Image();
+
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+
+      const pngFile = canvas.toDataURL('image/png');
+      const downloadLink = document.createElement('a');
+
+      downloadLink.download = `QR ${inputText}`;
+      downloadLink.href = pngFile;
+      downloadLink.click();
+    };
+
+    const svgBlob = new Blob([svgString], { type: 'image/svg+xml' });
+    const svgUrl = URL.createObjectURL(svgBlob);
+    img.src = svgUrl;
   };
 
   const closeCode = () => {
-    setIsShowCode(false);
     setInputText('');
   };
 
   return (
     <div className='app'>
       <h2>QR Code Generator</h2>
-      <div className='top-bar'>
-        <div className='input-titles'>
-          <h4 className='input-name'>Enter your text here</h4>
-          <h4 className='input-name'>Select size</h4>
-        </div>
-        <div className='input-fields'>
-          <input
-            className='input-data'
-            type='text'
-            onChange={(e) => setInputText(e.target.value)}
-            value={inputText}
-          />
-          <select className='input-data' onChange={(e) => setSize(e.target.value)}>
-            <option value={100}>100</option>
-            <option selected value={200}>
-              200
-            </option>
-            <option value={300}>300</option>
-            <option value={400}>400</option>
-            <option value={500}>500</option>
-            <option value={600}>600</option>
-            <option value={700}>700</option>
-            <option value={800}>800</option>
-            <option value={900}>900</option>
-            <option value={1000}>1000</option>
-          </select>
-        </div>
+      <div className='input-container'>
+        <label>Enter your text:</label>
+        <input type='text' onChange={(e) => setInputText(e.target.value)} value={inputText} />
+      </div>
+      <div className='input-container'>
+        <label>Select size:</label>
+        <select onChange={(e) => setSize(e.target.value)}>
+          <option value={100}>100</option>
+          <option value={200}>200</option>
+          <option value={300}>300</option>
+          <option value={400}>400</option>
+          <option value={500}>500</option>
+          <option value={600}>600</option>
+          <option value={700}>700</option>
+          <option value={800}>800</option>
+          <option value={900}>900</option>
+          <option value={1000}>1000</option>
+        </select>
       </div>
       <div className='buttons'>
-        <button className='btn' onClick={showCode}>
-          Generate
-        </button>
-        <button className='btn' onClick={closeCode}>
-          Clear
-        </button>
+        <button onClick={saveQR}>Save</button>
+        <button onClick={closeCode}>Clear</button>
       </div>
-      {isShowCode && inputText !== '' && (
+      {inputText !== '' && (
         <div className='qr-container'>
-          <QRCode size={size} value={inputText} viewBox={`0 0 256 256`} />
+          <QRCode
+            size={256}
+            style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
+            value={inputText}
+            viewBox={`0 0 256 256`}
+          />
         </div>
       )}
     </div>
